41c2686aa2819c6c8461e2087f0ddaff3b214593
Habibul Islam <habibulislam@accenture.com>
2017-04-24 13:50:35 -0400

    Adding temperature file to local repository

---
+++ Test_App/temp_converter_F-to-C.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace temp_converter_F_to_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fer, cel;
+
+            Console.WriteLine("Please type your fer value: ");
+
+            fer = int.Parse(Console.ReadLine());
+
+            cel = (5 * fer -160) / 9;
+
+            //Console.WriteLine("The " + fer + " degree fer is equal to " + cel + " cel.");
+            Console.WriteLine(cel);
+
+            Console.ReadLine();
+
+        }
+    }
+}
